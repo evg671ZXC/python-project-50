41f8a514cb262103daacf00692eed3bf1912cf87
evg671ZXC <evgmine671@gmail.com>
2024-03-17 01:24:33 +0600

    2 step completed

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -7,6 +7,8 @@ parser = argparse.ArgumentParser(
 parser.add_argument("first_file")
 parser.add_argument("second_file")
 
+parser.add_argument('-f', '--format', help="set format of output")
+
 args = parser.parse_args()
 print(args.echo)
 
