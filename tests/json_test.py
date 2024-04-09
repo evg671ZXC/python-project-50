c778d5ae84c105db63450f8015367bf94bc8b043
evg671ZXC <evgmine671@gmail.com>
2024-04-09 11:48:52 +0600

    lint errors fixed

--- tests/json_test.py
+++ tests/json_test.py
@@ -1,8 +1,5 @@
 import pytest
 from gendiff.scripts.cli import parse_args
-from tests.fixtures.verification import stylish_nested_check
-from tests.fixtures.verification import plain_nested_check
-from tests.fixtures.verification import json_nested_check
 from gendiff.formats.format_selector import generate_diff
 
 json_file1 = './tests/fixtures/nested/file1.json'
@@ -14,9 +11,9 @@ yaml_file2 = './tests/fixtures/nested/file2.yml'
 
 @pytest.mark.parametrize('flag, use_format, file1, file2, expected', [
     ('-f', 'stylish', '~/path/file1', '~/path/file2',
-    ('stylish', '~/path/file1', '~/path/file2')),
+        ('stylish', '~/path/file1', '~/path/file2')),
     ('-f', 'plain', '~/path/file1', '~/path/file2',
-    ('plain', '~/path/file1', '~/path/file2'))
+        ('plain', '~/path/file1', '~/path/file2'))
 ])
 def test_cli(flag, use_format, file1, file2, expected):
     args = parse_args([flag, use_format, file1, file2])
@@ -24,19 +21,29 @@ def test_cli(flag, use_format, file1, file2, expected):
     assert result == expected
 
 
-@pytest.mark.parametrize("file1, file2", [
+@pytest.mark.parametrize("format, file1, file2, verification", [
     (
+        "stylish",
         json_file1,
-        json_file2
+        json_file2,
+        "./tests/fixtures/stylish_nested_check"
     ),
     (
+        "plain",
         yaml_file1,
-        yaml_file2
+        yaml_file2,
+        "./tests/fixtures/plain_nested_check"
+    ),
+    (
+        "json",
+        yaml_file1,
+        yaml_file2,
+        "./tests/fixtures/json_nested"
     )
 ])
-def test_gendiff_output(file1, file2):
-    assert generate_diff(file1, file2, "stylish") == stylish_nested_check
-    assert generate_diff(file1, file2, "plain") == plain_nested_check
-    assert generate_diff(file1, file2, "json") == json_nested_check
+def test_gendiff_output(format, file1, file2, verification):
+    with open(verification) as f:
+        test_string = f.read()
     with pytest.raises(Exception):
         generate_diff(file1, file2, "FooBazz")
+    assert generate_diff(file1, file2, format) == test_string
